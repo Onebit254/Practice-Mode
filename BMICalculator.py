9bf6a99a0920cee0990a3837a2792a8ba9565fa7
Onebit_ke <68463244+Onebit254@users.noreply.github.com>
2023-07-26 05:02:33 +0300

    Add files via upload

---
+++ BMICalculator.py
@@ -0,0 +1,9 @@
+Weight = int(input("Weight: "))
+Height = int(input("Height: "))
+
+#Calculate BMI
+
+
+BMI = Weight / Height ** 2
+
+print(BMI)
---
+++ MoneyMarketCalculator.py
@@ -0,0 +1,9 @@
+#Calculate savings on interes in money market fund
+
+Savings = int(input("Savings: "))
+P_rate = float(input("P_rate: "))
+Years = float(input("Years: "))
+Interest = Savings * P_rate * Years
+
+#Interest on money saves on monthly basis
+print(Interest)
